eafde532215c5c9f143959f807d48574b1ae0d53
Medshi8 <72886413+Medshi8@users.noreply.github.com>
2020-11-22 13:18:34 +0100

    Add files via upload

---
+++ Lesson 4 Python Lists.py
@@ -0,0 +1,38 @@
+# bech ta3mel list jdida!
+l = []
+
+# bech ta3mel assignment (bech tsami values) 
+x = 5
+b = 'hello'
+c = 'Bye'
+
+# bech tzide el elemnts athokom lel List te3ek
+# exemple:  l.append(esm el value te3ek)
+l.append(x)
+l.append(y)
+l.append(c)
+
+# Slicing ( 9asan el list)
+# l2 new list 
+# ma3neha 9osli el list mel 0 lel 2 ama fiha just nomro 0 we 1 kahaw kima chofna fel video
+l2 = l[0:2] 
+
+# accessing list elements by index:
+# $Python Sell
+# >> l[0]
+# >> 5
+
+# nafs el list ama inversed :
+
+l3 = [::-1]
+""" 
+kifeh tchofe el methods (Skills ta3 el list wela ay object e5er)
+$Python Shell
+>> dir(l)
+>> [ lista fiha el methods mta3 el list]
+
+bech tchofe kifeh testa5dem method moa3yna:
+$Python Shell
+>> help(l.remove)
+>> yraja3lek kifeh testa5demha
+
