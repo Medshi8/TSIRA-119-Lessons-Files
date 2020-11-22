541132c890379f73e0ef7131e7afa45ec8f1106c
Medshi8 <72886413+Medshi8@users.noreply.github.com>
2020-11-22 12:37:42 +0100

    Add files via upload

---
+++ Lesson 3 Conditions_Logic.py
@@ -0,0 +1,29 @@
+x = 5
+b = 6
+z = 1
+if x > b:
+    print(' x > b')
+    
+elif x == b:
+    print(' x < b')
+    
+elif z == x:
+    print('z < x')
+    
+else:
+    print(' Nothing')
+    
+    
+"""
+7aja o5ra ma9otlhech fel video eli ki ta3mel assignment el Str we yeda ferag ma3neha atheka False 
+
+Exemple:
+
+x = ''
+y = 'hi'
+$Pyton Shell
+>>x and y 
+>> False 
+
+5ater Python El empty string 3ando howa  '' == False
+"""
